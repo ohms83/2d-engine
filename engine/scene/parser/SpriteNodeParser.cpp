b3401c8e72e5871cbb04a918afc9049e4bc91f9f
ohms <nuttachai@gumi.sg>
2021-06-28 20:50:22 +0800

    [Scene] Sprite parser to be able to create 9-sliced

--- engine/scene/parser/SpriteNodeParser.cpp
+++ engine/scene/parser/SpriteNodeParser.cpp
@@ -21,6 +21,36 @@ void SpriteNodeParser::parseAttributes(tinyxml2::XMLElement* element)
 
     NodeParser::parseAttributes(element);
 }
+
+bool SpriteNodeParser::parseElement(tinyxml2::XMLElement* element)
+{
+    const string elementName = element->Name();
+    // It's recommended that <resources> should be the top most tags; to prevent initialization errors.
+    if (elementName == "sliced")
+    {
+        parseSliced(element);
+        return true;
+    }
+    else
+    {
+        return NodeParser::parseElement(element);
+    }
+}
+
+void SpriteNodeParser::parseSliced(tinyxml2::XMLElement* element)
+{
+    Sprite* sprite = dynamic_cast<Sprite*>(m_node);
+    LOG_ASSERT(sprite, "m_node is not a cocos2d::Sprite!");
+    
+    Rect centerRect;
+    element->QueryFloatAttribute("x", &centerRect.origin.x);
+    element->QueryFloatAttribute("y", &centerRect.origin.y);
+    element->QueryFloatAttribute("w", &centerRect.size.width);
+    element->QueryFloatAttribute("h", &centerRect.size.height);
+
+    sprite->setCenterRect(centerRect);
+}
+
 Node* SpriteNodeParser::createNode()
 {
     return Sprite::create();
